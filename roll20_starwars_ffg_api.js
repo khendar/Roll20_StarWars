837ade4be1e30775035d7c390c3c794d4238e80f
Tim Parkinson <khendar@gmail.com>
2016-08-03 15:59:53 +0930

    Removed includes polyfill and replaced with indexOf
    Fixes issue with Critical Hit rolls not working

--- roll20_starwars_ffg_api.js
+++ roll20_starwars_ffg_api.js
@@ -121,35 +121,6 @@
  */
 
 /* Define functions that may not always exist */
-if (!Array.prototype.includes) {
-    Array.prototype.includes = function(searchElement /*, fromIndex*/ ) {
-        'use strict';
-        var O = Object(this);
-        var len = parseInt(O.length, 10) || 0;
-        if (len === 0) {
-            return false;
-        }
-        var n = parseInt(arguments[1], 10) || 0;
-        var k;
-        if (n >= 0) {
-            k = n;
-        } else {
-            k = len + n;
-            if (k < 0) {k = 0;}
-        }
-        var currentElement;
-        while (k < len) {
-            currentElement = O[k];
-            if (searchElement === currentElement ||
-              (searchElement !== searchElement && currentElement !== currentElement)) { // NaN !== NaN
-                return true;
-            }
-            k++;
-        }
-        return false;
-    };
-}
-
 if(!log) {
     log = function(input) {
         console.log(input);
@@ -1222,7 +1193,7 @@ function SuggestionEngine () {
                     diceObj = addSkillSuggestionsFromArray(diceObj, skill[symbol], symbol, rollResultForSymbol, diceObj.vars.suggestions.general);
                 }
 
-                if (fearJSON && fearJSON.allowedSkills.includes(skillName)) {
+                if (fearJSON && fearJSON.allowedSkills.indexOf(skillName)) {
                     diceObj = addSkillSuggestionsFromArray(diceObj, fearJSON[symbol], symbol, rollResultForSymbol, diceObj.vars.suggestions.general);
                 }
             }
